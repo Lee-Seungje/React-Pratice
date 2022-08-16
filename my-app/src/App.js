c38affbb4b45e5842d7b96dabd2ea3886f1cecf1
Seungjee Lee <moondgod@gmail.com>
2022-08-16 22:30:18 +0900

    practice1

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div>
+      <hi className={styles.title}>Welcome back!!!</hi>
+      <Button text={"continue"}/>
+    </div>
+  );
+}
+
+
+
+export default App;
